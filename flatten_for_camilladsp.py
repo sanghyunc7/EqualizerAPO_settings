dd3f0620cba6a33fba0683f4a859e6178232b6fc
dan choi <sanghyunc7@gmail.com>
2024-04-21 00:39:24 -0400

    translator txt to yml

--- flatten_for_camilladsp.py
+++ flatten_for_camilladsp.py
@@ -1,51 +1,56 @@
 import sys
 
-def flatten_equalizer(file_path):
-    flat_contents = []
-    with open(file_path, 'r') as f:
-        for line in f:
-            line = line.strip()
-            if not line.startswith('#'):  # Skip commented-out lines
-                if line.startswith('Include:'):
-                    include_file = line.split(': ')[1]
-                    included_contents = flatten_equalizer(include_file)
-                    flat_contents.extend(included_contents)
-                else:
-                    flat_contents.append(line)
-    return flat_contents
+
+class Flat_TXT:
+    def __init__(self, input_file) -> None:
+        self.file = input_file
+        self.contents = []
+
+    # unsorted output
+    def flatten_equalizer(self, include_file):
+        with open(include_file, "r") as f:
+            for line in f:
+                line = line.strip()
+                if not line.startswith("#"):  # Skip commented-out lines
+                    if line.startswith("Include:"):
+                        child_file = line.split(": ")[1]
+                        self.flatten_equalizer(child_file)
+                    else:
+                        self.contents.append(line)
+
+    def sort_filters(self):
+        new_contents = []
+        for line in self.contents:
+            if line.startswith("Filter:"):
+                words = line.split()
+                frequency = words[words.index("Hz") - 1]
+                new_contents.append((float(frequency), line))
+            else:
+                new_contents.append((30000, line))
+        new_contents.sort()
+        self.contents = []
+        for k, v in new_contents:
+            self.contents.append(v)
+
+    def get_flat(self):
+        if self.contents:
+            return self.contents
+        self.flatten_equalizer(self.file)
+        self.sort_filters()
+        return self.contents
 
 
-def sort_filters(contents):
-    new_contents = []
-    for line in contents:
-        if line.startswith("Filter:"):
-            words = line.split()
-            frequency = words[words.index('Hz') - 1]
-            new_contents.append((float(frequency), line))
-        else:
-            new_contents.append((0, line))
-    new_contents.sort()
-    rtn = []
-    for k,v in new_contents:
-        rtn.append(v)
-    return rtn
+# def main():
+#     input_file = sys.argv[1]
+#     output_file = "flattened_" + input_file
+#     flat_contents = flatten_equalizer(input_file)
+#     print(flat_contents)
+#     flat_contents = sort_filters(flat_contents)
+#     print(flat_contents)
+#     with open(output_file, 'w') as f:
+#         for line in flat_contents:
+#             f.write(line + '\n')
 
 
-def main():
-    input_file = sys.argv[1]
-    output_file = "flattened_" + input_file
-    flat_contents = flatten_equalizer(input_file)
-    print(flat_contents)
-    flat_contents = sort_filters(flat_contents)
-    print(flat_contents)
-    with open(output_file, 'w') as f:
-        for line in flat_contents:
-            f.write(line + '\n')
-
-
-
-
-
-
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
---
+++ translator.py
@@ -0,0 +1,94 @@
+# first flatten eqapo
+# then using dsp_template, create json
+# then use json->yml library to make yml output
+import sys
+import os
+import yaml
+import json
+from copy import deepcopy
+
+from flatten_for_camilladsp import Flat_TXT
+
+
+filter_template = {
+    "description": "",
+    "parameters": {"freq": 100, "gain": 0, "q": 1, "type": "Peaking"},
+    "type": "Biquad",
+}
+
+preamp_template = {
+    "description": "",
+    "parameters": {"gain": 0, "scale": "dB"},
+    "type": "Gain",
+}
+
+
+class Translator:
+    def __init__(self, input_file, output_file):
+        self.txt = Flat_TXT(input_file).get_flat()
+        self.output_file = output_file
+        self.yml = None
+        with open("dsp_template.json", "r") as file:
+            self.jsn = json.load(file)
+
+    # expects array of str
+    # outputs json that adheres to dsp formatting
+
+    def extract_data(self, txt_field):
+        words = txt_field.split()
+        d = {}
+        if "Preamp:" == words[0]:
+            d["data_type"] = "Preamp"
+            d["gain"] = words[1]
+        else:
+            d["data_type"] = "Filter"
+            d["type"] = "Peaking"
+            if words[2] == "LSC":
+                d["type"] = "Lowshelf"
+            elif words[2] == "HSC":
+                d["type"] = "Highshelf"
+            d["freq"] = float(words[4])
+            d["gain"] = float(words[7])
+            d["q"] = float(words[-1])
+        return d
+
+    def padded(self, i, padding):
+        left_zeros = padding - len(str(i))
+        return "0" * left_zeros + str(i)
+
+    def txt_to_json(self):
+        padding = len(str(len(self.txt)))
+
+        for i, v in enumerate(self.txt):
+            d = self.extract_data(v)
+            if d["data_type"] == "Preamp":
+                config = deepcopy(preamp_template)
+                name = "Preamp_" + self.padded(i, padding)
+                config["description"] = v
+                config["parameters"]["gain"] = d["gain"]
+            else:
+                name = "Filter_" + self.padded(i, padding)
+                config = deepcopy(filter_template)
+                config["description"] = v
+                config["parameters"]["freq"] = d["freq"]
+                config["parameters"]["gain"] = d["gain"]
+                config["parameters"]["q"] = d["q"]
+                config["parameters"]["type"] = d["type"]
+            self.jsn["filters"][name] = config
+            self.jsn["pipeline"][1]["names"].append(name)  # channel 1 left ear
+            self.jsn["pipeline"][2]["names"].append(name)  # channel 2 right ear
+
+    def json_to_yaml(self):
+        with open(self.output_file, "w") as yaml_file:
+            yaml.dump(self.jsn, yaml_file)
+
+
+if __name__ == "__main__":
+    input_file = sys.argv[1]
+    if not input_file.endswith(".txt"):
+        print("input text file")
+        sys.exit(1)
+    output_file = input_file.rstrip(".txt") + ".yml"
+    translator = Translator(input_file, output_file)
+    translator.txt_to_json()
+    translator.json_to_yaml()
