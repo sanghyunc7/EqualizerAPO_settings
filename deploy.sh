74be66770f7f26eb1ee2b4c5fa570b452d097c81
dan choi <dan@windows_pc>
2024-05-10 23:46:49 -0400

    commit message option

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,28 @@ if [ "$#" -gt 0 ]; then
     commit_msg="$@"
 fi
 git commit -m "$commit_msg"
+git push origin master
 
+ssh -t admin@raspberrypi.local << 'END_SCRIPT'
+
+    target_dir="/home/admin/EqualizerAPO_settings"
+    if [ ! -d "$target_dir" ]; then
+        echo "Directory $target_dir does not exist. Cloning..."
+        git clone https://github.com/sanghyunc7/EqualizerAPO_settings.git "$target_dir"
+        if [ $? -ne 0 ]; then
+            echo "Error: Git clone failed!"
+            exit 1
+        fi
+    fi
+
+    cd "$target_dir"
+    git pull --ff-only
+    cp *.yml /usr/share/camilladsp/configs/
+    sudo systemctl restart mpd.service && mpc play
+    echo "DONE"
+
+    exit
+    
+END_SCRIPT
 
 
